87d551572001b702c1c65649639cfce22a87ec6f
Mohamed-Selmi <mohamedamineselmi@outlook.com>
2025-12-05 22:28:18 +0100

    Create the hotel entity and form Working and implementing the image attribute

---
+++ src/Entity/Hotel.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HotelRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HotelRepository::class)]
+class Hotel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $capacity = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: '0')]
+    private ?string $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $hotelPicture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): static
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getHotelPicture(): ?string
+    {
+        return $this->hotelPicture;
+    }
+
+    public function setHotelPicture(string $hotelPicture): static
+    {
+        $this->hotelPicture = $hotelPicture;
+
+        return $this;
+    }
+}
---
+++ src/Form/HotelType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Hotel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Compnenet\Form\Extension\Core\Type\SubmitType;
+class HotelType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('capacity')
+            ->add('price')
+            ->add('hotelPicture',FileType::class,['label'=>'Hotel picture (image)',
+            'mapped'=>false,
+            'required'=>true,
+            'constraints'=>[
+                new File(
+                    extensions:['jpg','png','jpeg'],
+                    extensionsMessage:'Please upload a valid Image',
+                )
+                ],
+            ])
+            ->add('Validate',SubmityType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Hotel::class,
+        ]);
+    }
+}
